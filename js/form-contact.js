3397af60c7659502c7e486973100466b04eb3b47
Celso Bastos <celsorico@gmail.com>
2024-09-04 19:33:49 -0300

    aula 13

--- js/form-contact.js
+++ js/form-contact.js
@@ -4,8 +4,6 @@
     const btnSubmit = document.querySelector('[data-js="btn-contact-submit"]');
     const campos = document.querySelectorAll('[data-js="valida-form"]');
     
-    const Cadastro = {}
-
     btnSubmit.addEventListener('click', event => {
         event.preventDefault();
         let camposEstaoPreenchidos = true;
@@ -15,23 +13,10 @@
                 camposEstaoPreenchidos = false; 
                 element.classList.add('error');
             }
-
-            let name = element.getAttribute('name');
-            Cadastro[name] = element.value;
         });
 
-        const cadastro = JSON.stringify(Cadastro);
-        localStorage.setItem('cadastro', cadastro);
-        
         if (camposEstaoPreenchidos) {
             form.submit();
         }
     });
-
-    let getCadastro = JSON.parse(localStorage.getItem('cadastro'));
-
-    form[0].value = getCadastro.nome
-    form[1].value = getCadastro.email
-    form[2].value = getCadastro.comentario
-
 })()
